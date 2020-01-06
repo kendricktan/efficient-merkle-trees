809e3ad2c031926cb27e2e74de551b449100efc2
Kendrick Tan <kendricktan0814@gmail.com>
2020-01-06 10:26:47 +1000

    merkletree viz

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import { 
   Container,
@@ -7,21 +7,23 @@ import {
   Label,
   Button,
   Input,
-  Form,
-  FormGroup,
   Dropdown,
   DropdownToggle,
   DropdownMenu,
   DropdownItem
 } from 'reactstrap';
 
-import { Stage, Layer, Text, Rect } from 'react-konva';
-import Konva from 'konva';
+import { Stage, Layer, Text, Group, Rect } from 'react-konva';
 
 import { bigInt } from 'snarkjs'
 import { createMerkleTree } from './utils/merkletree.js'
 
+import bonsai from './bonsai.png'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const nodeWidth = 90
+const nodeHeight = 30
 
 const ActionTypes = {
   Insert: "Insert",
@@ -35,12 +37,14 @@ const AppSettings = () => {
 
   return (
     <div style={{ paddingTop: '30px' }}>
+      <img alt="logo" src={bonsai} height="48" style={{ float: 'left', paddingRight: '30px' }}/>
+      <h1>Efficient Merkle Tree Visualization</h1>
       <hr />
-      <Form>
-        <FormGroup>
+      <Row>
+        <Col sm="4">
           <Label>Action</Label>
           <Dropdown isOpen={dropdownOpen} toggle={() => setDropdownOpen(!dropdownOpen)}>
-            <DropdownToggle caret>
+            <DropdownToggle block caret>
               {action}
             </DropdownToggle>
             <DropdownMenu>
@@ -60,40 +64,126 @@ const AppSettings = () => {
               }
             </DropdownMenu>
           </Dropdown>
-        </FormGroup>
-        <FormGroup>
+        </Col>
+        <Col sm="4">
           <Label>Leaf Index</Label>
           <Input disabled={action === ActionTypes.Insert} placeholder="0" />
-        </FormGroup>
-        <FormGroup>
+        </Col>
+        <Col sm="4">
           <Label>Leaf Value</Label>
           <Input placeholder="0" />
-        </FormGroup>
-        <FormGroup>
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col sm="12">
           <Button block>
             Go
           </Button>
-        </FormGroup>
-      </Form>
+        </Col>
+      </Row>
+
       <hr />
     </div>
   )
 }
 
-const ColoredRect = () => {
-  const [color, setColor] = useState('green')
+const TreeNode = ({text, x, y}) => {
+  let t
+
+  if (text === '0') {
+    t = text
+  } else {
+    t = text.slice(0, 8) + '..'
+  }
 
   return (
-    <Rect
-      x={20}
-      y={20}
-      width={50}
-      height={50}
-      fill={color}
-      shadowBlur={5}
-      onClick={() => setColor(Konva.Util.getRandomColor())}
-    />
+    <Group>
+      <Rect
+        x={x}
+        y={y}
+        width={nodeWidth}
+        height={nodeHeight}
+        stroke={'black'}
+        onClick={(x) => console.log(x)}
+      />
+      <Text text={t} fontSize={15} x={t === '0' ? x + nodeWidth/2 - 5 : x + 10} y={y + 9}/>
+    </Group>
   );
+}
+
+const DynamicSizedCanvas = () => {
+  const [width, setWidth] = useState(0)
+
+  const minWidth = width > 900 ? width : 900
+  const height = 500
+
+  const m = createMerkleTree(4, bigInt(0))
+
+  const renderMerkleTree = () => {
+    let nodes = []
+
+    for (let i = 0; i < m.depth; i++) {
+      const itemsInLevel = Math.pow(2, i)
+
+      if (i === 0) {
+        // Root
+        nodes.push(<TreeNode text={m.root.toString(16)} x={width/2 - nodeWidth/2} y={40} />)
+      } else if (i < m.depth - 1) {
+        // Subtrees
+        for (let j = 0; j < itemsInLevel; j++) {
+          let t
+
+          try {
+            t = m.filledPaths[i][j].toString(16)
+          } catch {
+            t = m.filledSubtrees[i].toString(16)
+          }
+
+          const paddingX = parseInt(width/(1 + itemsInLevel))
+
+          nodes.push(
+            <TreeNode text={t} x={((j+1)*paddingX) - (nodeWidth/2)} y={40+(i*60)} />
+          )
+        }
+      } else {
+        // Leaves
+        for (let j = 0; j < itemsInLevel; j++) {
+          let t
+
+          try {
+            t = m.leaves[j].toString(16)
+          } catch {
+            t = m.zeroValue.toString(16)
+          }
+
+          const paddingX = parseInt(width/(1 + itemsInLevel))
+
+          nodes.push(
+            <TreeNode text={t} x={((j+1)*paddingX) - (nodeWidth/2)} y={40+(i*60)} />
+          )
+        }
+      }
+    }
+
+    return nodes
+  }
+
+  return (
+    <div style={{ minWidth: minWidth, width: '100%', height: '100%'}} ref={useCallback((node) => { 
+      if (node !== null) {
+        setWidth(node.getBoundingClientRect().width);
+      }
+    }, [])}>
+      <Stage width={width} height={height}>
+        <Layer>
+        {
+          renderMerkleTree()
+        }
+      </Layer>
+      </Stage>
+    </div>
+  )
 }
 
 
@@ -105,12 +195,7 @@ const App = () => {
           <AppSettings />
         </Col>
         <Col sm="12" md="12">
-          <Stage width={window.innerWidth} height={window.innerHeight}>
-            <Layer>
-              <Text text="Try click on rect" />
-              <ColoredRect />
-            </Layer>
-          </Stage>
+          <DynamicSizedCanvas />
         </Col>
       </Row>
     </Container>
